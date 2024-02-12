1385e29dd169ac1d63e1a1c19d9ab6df2d512d1a
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2024-02-11 23:18:24 -0600

    add comments

--- Ejercicios/passwordCheckerv2.cpp
+++ Ejercicios/passwordCheckerv2.cpp
@@ -10,15 +10,26 @@ int main(int argc, char const *argv[])
     // char password[5] = {'1', '2', '3', '4', '\0'};
     // char password[4] = {'1', '2', '3', '4'};
     char password[5] = {"1234"};
-    cout << "Password[2]: " << password[2] << endl;
-    cout << "Password[0]: " << password[0] << endl;
+    cout << "Password[2]: " << password[2] << endl; // Que valor toma?
+    cout << "Password[0]: " << password[0] << endl; // Que valor toma?
     cout << "Password: " << password << endl;
 
-    string passwordString = "1234";
-    cout << "PasswordString[2]: " << passwordString[2] << endl;
-    cout << "PasswordString[0]: " << passwordString[0] << endl;
+    string passwordString = password;                           // Son equivalentes?
+    cout << "PasswordString[2]: " << passwordString[2] << endl; // Que valor toma?
+    cout << "PasswordString[0]: " << passwordString[0] << endl; // Que valor toma?
+    cout << "PasswordString: " << passwordString << endl;
+
     // cout << "PasswordString[60]: " << passwordString[60] << endl;
     // cout << "Password[60]: " << password[60] << endl;
+
+    string verificarString;
+    // char verificarString[5];
+
+    cout << "Ingrese password para ingresar: " << endl;
+    cin >> verificarString;
+    cout << "Password ingresada: " << verificarString << endl;
+    // cout << "Tamanio: " << verificarString.length() << endl;
+    // cout << "Comparar: " << verificarString.compare("1234") << endl;
 
     return 0;
 }
