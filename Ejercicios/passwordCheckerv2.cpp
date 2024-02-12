223609c3aa317ca491a85d2e04873d43cc51d50d
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2024-02-11 22:26:49 -0600

    init arrays

--- Ejercicios/passwordCheckerv2.cpp
+++ Ejercicios/passwordCheckerv2.cpp
@@ -6,5 +6,19 @@ int main(int argc, char const *argv[])
 {
     int intentos = askForRetries();
     cout << "Intentos: " << intentos << endl;
+
+    // char password[5] = {'1', '2', '3', '4', '\0'};
+    // char password[4] = {'1', '2', '3', '4'};
+    char password[5] = {"1234"};
+    cout << "Password[2]: " << password[2] << endl;
+    cout << "Password[0]: " << password[0] << endl;
+    cout << "Password: " << password << endl;
+
+    string passwordString = "1234";
+    cout << "PasswordString[2]: " << passwordString[2] << endl;
+    cout << "PasswordString[0]: " << passwordString[0] << endl;
+    // cout << "PasswordString[60]: " << passwordString[60] << endl;
+    // cout << "Password[60]: " << password[60] << endl;
+
     return 0;
 }
