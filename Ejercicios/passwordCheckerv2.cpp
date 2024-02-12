66af476f32d82aa900ceaf9c74a186f1c840af7e
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2024-02-11 21:26:10 -0600

    punteros

--- Ejercicios/passwordCheckerv2.cpp
+++ Ejercicios/passwordCheckerv2.cpp
@@ -5,6 +5,15 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     int intentos = askForRetries();
-    cout << "Intentos: " << intentos << endl;
+    cout << "Valor Intentos: " << intentos << endl;
+    cout << "Direccion de Intentos: " << &intentos << endl;
+
+    int *soyUnPuntero = &intentos;
+    cout << "Direccion de puntero: " << soyUnPuntero << endl;
+    cout << "Valor de puntero: " << *soyUnPuntero << endl;
+    soyUnPuntero++;
+    cout << "Direccion de puntero despues de incremetar: " << soyUnPuntero << endl;
+    cout << "Valor de puntero despues de incrementar: " << *soyUnPuntero << endl;
+
     return 0;
 }
